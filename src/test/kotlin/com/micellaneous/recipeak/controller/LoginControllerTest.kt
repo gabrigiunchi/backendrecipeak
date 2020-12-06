ff9b9a9fbb914468112f036f8413c221b2ce23be
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2020-12-06 17:11:40 +0100

    modern approach to mockMvc

--- src/test/kotlin/com/micellaneous/recipeak/controller/LoginControllerTest.kt
+++ src/test/kotlin/com/micellaneous/recipeak/controller/LoginControllerTest.kt
@@ -8,14 +8,13 @@ import com.micellaneous.recipeak.model.dto.input.ValidateTokenRequest
 import com.micellaneous.recipeak.model.dto.input.ValidateUserDTO
 import com.micellaneous.recipeak.model.enum.UserType
 import org.assertj.core.api.Assertions.assertThat
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.notNullValue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.post
 
 class LoginControllerTest : BaseRestTest() {
 
@@ -38,19 +37,18 @@ class LoginControllerTest : BaseRestTest() {
         assertThat(this.userDAO.findByUsername(user.username)?.password).isEqualTo(user.password)
         assertThat(this.userDAO.findByUsername(user.username)?.active).isTrue()
 
-        this.mockMvc.perform(
-            post(ApiUrls.LOGIN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json(credentials))
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.user.id", Matchers.`is`(user.id)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.user.name", Matchers.`is`(user.name)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.user.surname", Matchers.`is`(user.surname)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.user.username", Matchers.`is`(user.username)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.user.email", Matchers.`is`(user.email)))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.token", Matchers.notNullValue()))
-            .andDo(MockMvcResultHandlers.print())
+        mockMvc.post(ApiUrls.LOGIN)
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = json(credentials)
+        }
+            .andExpect { status { isOk() } }
+            .andExpect { jsonPath("$.user.id", `is`(user.id)) }
+            .andExpect { jsonPath("$.user.name", `is`(user.name)) }
+            .andExpect { jsonPath("$.user.surname", `is`(user.surname)) }
+            .andExpect { jsonPath("$.user.username", `is`(user.username)) }
+            .andExpect { jsonPath("$.user.email", `is`(user.email)) }
+            .andExpect { jsonPath("$.token", notNullValue()) }
     }
 
     @Test
@@ -59,19 +57,13 @@ class LoginControllerTest : BaseRestTest() {
 
         assertThat(this.userDAO.findByUsername(credentials.username) == null).isTrue()
 
-        this.mockMvc.perform(
-            post(ApiUrls.LOGIN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json(credentials))
-        )
-            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
-            .andExpect(
-                MockMvcResultMatchers.jsonPath(
-                    "$.message",
-                    Matchers.`is`("Invalid username/password supplied")
-                )
-            )
-            .andDo(MockMvcResultHandlers.print())
+        mockMvc.post(ApiUrls.LOGIN)
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = json(credentials)
+        }
+            .andExpect { status { isUnauthorized() } }
+            .andExpect { jsonPath("$.message", `is`("Invalid username/password supplied")) }
     }
 
     @Test
@@ -80,19 +72,13 @@ class LoginControllerTest : BaseRestTest() {
         val user = this.createMockUser("djkasnjdna", password)
         val credentials = ValidateUserDTO(user.username, "mmkldamaldmak")
 
-        this.mockMvc.perform(
-            post(ApiUrls.LOGIN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json(credentials))
-        )
-            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
-            .andExpect(
-                MockMvcResultMatchers.jsonPath(
-                    "$.message",
-                    Matchers.`is`("Invalid username/password supplied")
-                )
-            )
-            .andDo(MockMvcResultHandlers.print())
+        mockMvc.post(ApiUrls.LOGIN)
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = json(credentials)
+        }
+            .andExpect { status { isUnauthorized() } }
+            .andExpect { jsonPath("$.message", `is`("Invalid username/password supplied")) }
     }
 
     @Test
@@ -101,25 +87,25 @@ class LoginControllerTest : BaseRestTest() {
         this.userDAO.save(user)
         val token = this.tokenProvider.createToken(user.username, listOf(UserType.ADMINISTRATOR.name))
 
-        this.mockMvc.perform(
-            post("${ApiUrls.LOGIN}/token")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json(ValidateTokenRequest(token)))
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.valid", Matchers.`is`(true)))
+        mockMvc.post("${ApiUrls.LOGIN}/token")
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = json(ValidateTokenRequest(token))
+        }
+            .andExpect { status { isOk() } }
+            .andExpect { jsonPath("$.valid", `is`(true)) }
     }
 
     @Test
     fun `Should say if a token is NOT valid`() {
         val token = ValidateTokenRequest("dadsajs")
-        this.mockMvc.perform(
-            post("${ApiUrls.LOGIN}/token")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json(token))
-        )
-            .andExpect(MockMvcResultMatchers.status().isOk)
-            .andExpect(MockMvcResultMatchers.jsonPath("$.valid", Matchers.`is`(false)))
+        mockMvc.post("${ApiUrls.LOGIN}/token")
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = json(token)
+        }
+            .andExpect { status { isOk() } }
+            .andExpect { jsonPath("$.valid", `is`(false)) }
     }
 
     @Test
@@ -131,18 +117,13 @@ class LoginControllerTest : BaseRestTest() {
 
         val credentials = ValidateUserDTO(user.username, password)
 
-        this.mockMvc.perform(
-            post(ApiUrls.LOGIN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json(credentials))
-        )
-            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
-            .andExpect(
-                MockMvcResultMatchers.jsonPath(
-                    "$.message",
-                    Matchers.`is`("Invalid username/password supplied")
-                )
-            )
+        mockMvc.post(ApiUrls.LOGIN)
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = json(credentials)
+        }
+            .andExpect { status { isUnauthorized() } }
+            .andExpect { jsonPath("$.message", `is`("Invalid username/password supplied")) }
     }
 
 }
